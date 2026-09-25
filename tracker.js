5f4b9fd5a42f22e0eb36bb869cbc782d24ae9719
kimsonharapan-cpu <kimsonharapan@gmail.com>
2026-09-25 21:49:20 +0700

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,12 +1,12 @@
 // ====== KODE TRACKER MURNI JAVASCRIPT ======
-var sc_project = 13356722;
+var sc_project = 13356818;
 var sc_invisible = 1;
-var sc_security = "f0ba7a8e";
-var scJsHost = "https://";
+var sc_security = "815567e7";
 
-// Memuat script utama Statcounter secara otomatis dan aman
+// Memuat script utama Statcounter secara dinamis
 var scScript = document.createElement('script');
 scScript.type = 'text/javascript';
 scScript.async = true;
-scScript.src = scJsHost + '://statcounter.com';
+scScript.src = 'https://www.statcounter.com/counter/counter.js';
+
 document.head.appendChild(scScript);
