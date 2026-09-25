28024ed4c34c8e080a529e40564a2960c2dabbc3
kimsonharapan-cpu <kimsonharapan@gmail.com>
2026-09-25 15:13:22 +0700

    Create tracker.js

---
+++ tracker.js
@@ -0,0 +1,12 @@
+// ====== KODE TRACKER MURNI JAVASCRIPT ======
+var sc_project = 13356722;
+var sc_invisible = 1;
+var sc_security = "f0ba7a8e";
+var scJsHost = "https://";
+
+// Memuat script utama Statcounter secara otomatis dan aman
+var scScript = document.createElement('script');
+scScript.type = 'text/javascript';
+scScript.async = true;
+scScript.src = scJsHost + '://statcounter.com';
+document.head.appendChild(scScript);
